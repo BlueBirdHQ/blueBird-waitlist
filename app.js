9dba3538dc16abe74eb75e422b31ed020ad514d4
Shivraj <shivunofficial003@gmail.com>
2022-07-15 15:52:46 +0530

    links in new tab

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const waitlist=document.querySelector('.btn-waitlist')
 console.log(waitlist)
 waitlist.addEventListener('click',()=>location.href='./form.html')
 
-document.querySelector('.github').addEventListener('click',()=>location.href='https://github.com/BlueBirdHQ')
-document.querySelector('.twitter').addEventListener('click',()=>location.href='https://twitter.com/BlueBirdHQ_')
+document.querySelector('.github').addEventListener('click',()=>window.open('https://github.com/BlueBirdHQ', '_blank'))
+document.querySelector('.twitter').addEventListener('click',()=>window.open('https://twitter.com/BlueBirdHQ_', '_blank'))
 document.querySelector('.discord').addEventListener('click',()=>location.href='')
+
+
+
