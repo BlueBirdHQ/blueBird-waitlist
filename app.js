5ae6335c2d7054fc6b1d99fbfe7764c6f638e783
suriya0304 <ksuriya0304@gmail.com>
2022-07-15 15:05:16 +0530

    waitlist and socials

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const waitlist=document.querySelector('.btn-waitlist')
+
+console.log(waitlist)
+waitlist.addEventListener('click',()=>location.href='./form.html')
+
+document.querySelector('.github').addEventListener('click',()=>location.href='https://github.com/BlueBirdHQ')
+document.querySelector('.twitter').addEventListener('click',()=>location.href='https://twitter.com/BlueBirdHQ_')
+document.querySelector('.discord').addEventListener('click',()=>location.href='')
